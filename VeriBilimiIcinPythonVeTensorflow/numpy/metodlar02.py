1879ada78d6b1d530652c46837dafa0e1e1c9f01
Sena Nur Özcan <1.senaozcan@gmail.com>
2024-09-12 11:25:59 +0100

    Create metodlar02.py

---
+++ VeriBilimiIcinPythonVeTensorflow/numpy/metodlar02.py
@@ -0,0 +1,11 @@
+## random
+
+np.random.randn(8)
+np.random.randn(4,4)
+np.random.randint(1,10)
+np.random.randint(1,10,5)
+myNumpy = np.arange(30)
+myNumpy   ## array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
+                     17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29])
+myRandom = np.random.randint(1,100,30)
+myRandom
