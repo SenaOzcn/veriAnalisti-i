f57c7f28511173937909f9375606d95cc37361c6
Sena Nur Özcan <1.senaozcan@gmail.com>
2024-09-16 10:18:38 +0100

    Update Series.py

--- VeriBilimiIcinPythonVeTensorflow/Pandas/Series.py
+++ VeriBilimiIcinPythonVeTensorflow/Pandas/Series.py
@@ -41,3 +41,47 @@ pd.Series(numRange,myNames)   ## Atıl      50
                               ## Zeynep    40
                               ## Mehmet    30
                               ## dtype: int64
+
+comp1 = pd.Series([10, 5, 1] ,["Atıl", "Atlas", "Osman"])
+comp1   ## Atıl     10
+        ## Atlas     5
+        ## Osman     1
+        ## dtype: int64
+
+comp2 = pd.Series([20, 10, 8] ,["Atıl", "Atlas", "Osman"])
+comp2   ## Atıl     20
+        ## Atlas    10
+        ## Osman     8
+        ## dtype: int64
+
+comp2["Atlas"]   ## 10
+
+result = comp1 + comp2
+result  ## Atıl     30
+        ## Atlas    15
+        ## Osman     9
+        ## dtype: int64
+
+difSeries = pd.Series([20, 30, 40, 50], ["a", "b", "c", "d"])
+difSeries2 = pd.Series([10, 5, 3, 1], ["a", "c", "f", "g"])
+
+difSeries   ## a    20
+            ## b    30
+            ## c    40
+            ## d    50
+            ## dtype: int64
+
+difSeries2  ## a    10
+            ## c     5
+            ## f     3
+            ## g     1
+            ## dtype: int64
+
+difSeries + difSeries2    ## a    30.0
+                          ## b     NaN
+                          ## c    45.0
+                          ## d     NaN
+                          ## f     NaN
+                          ## g     NaN
+                          ## dtype: float64
+
