f6778ef672ae4cd093603296ca2db428d1174157
Sena Nur Özcan <1.senaozcan@gmail.com>
2024-09-18 10:21:41 +0100

    Update DataFramePandas.py

--- VeriBilimiIcinPythonVeTensorflow/Pandas/DataFramePandas.py
+++ VeriBilimiIcinPythonVeTensorflow/Pandas/DataFramePandas.py
@@ -68,3 +68,72 @@ newDataFrame.iloc[1]
 ## Age             -0.257199
 ## Working Hours    1.156879
 ## Name: Zeynep, dtype: float64
+
+newDataFrame["Retirement Age"] = newDataFrame["Age"] + newDataFrame["Age"]
+newDataFrame
+##               Salary	           Age	    Working Hours     Retirement Age
+## Atıl	        0.464844	 0.281287	0.947364	 0.562574
+## Zeynep	1.126790	-0.257199	1.156879	-0.514398
+## Atlas	2.046861	0.138559	-0.089185	0.277117
+## Mehmet	0.378610	-1.382907	0.139203	-2.765814
+
+newDataFrame.drop("Retirement Age", axis = 1)
+##              Salary	           Age	    Working Hours
+## Atıl	        0.464844	0.281287	0.947364
+## Zeynep	1.126790	-0.257199	1.156879
+## Atlas	2.046861	0.138559	-0.089185
+## Mehmet	0.378610	-1.382907	0.139203
+
+newDataFrame.drop("Mehmet", axis = 0)
+## 	          Salary	  Age	     Working Hours   Retirement Age
+## Atıl	        0.464844	0.281287	0.947364	0.562574
+## Zeynep	1.126790	-0.257199	1.156879	-0.514398
+## Atlas	2.046861	0.138559	-0.089185	0.277117
+
+newDataFrame.drop("Retirement Age", axis = 1, inplace = True)
+##              Salary	           Age	    Working Hours
+## Atıl	        0.464844	0.281287	0.947364
+## Zeynep	1.126790	-0.257199	1.156879
+## Atlas	2.046861	0.138559	-0.089185
+## Mehmet	0.378610	-1.382907	0.139203
+
+newDataFrame.loc["Atıl"]
+## Salary           0.464844
+## Age              0.281287
+## Working Hours    0.947364
+## Name: Atıl, dtype: float64
+
+newDataFrame.loc["Atıl"]["Age"]
+## 0.28128691332222155
+
+newDataFrame.loc["Atlas", "Age"]
+## 0.13855854676563284
+
+newDataFrame < 0
+## 	        Salary	Age	Working Hours
+## Atıl	        False	False	False
+## Zeynep	False	True	False
+## Atlas	False	False	True
+## Mehmet	False	True	False
+
+booleanFrame = newDataFrame < 0
+booleanFrame
+## 	        Salary	Age	Working Hours
+## Atıl	        False	False	False
+## Zeynep	False	True	False
+## Atlas	False	False	True
+## Mehmet	False	True	False
+
+newDataFrame[booleanFrame]
+## 	        Salary	Age	Working Hours
+## Atıl	        NaN	NaN	NaN
+## Zeynep	NaN	-0.257199 NaN
+## Atlas	NaN	NaN	-0.089185
+## Mehmet	NaN	-1.382907 NaN
+
+newDataFrame[newDataFrame < 0]
+## 	        Salary	Age	Working Hours
+## Atıl	        NaN	NaN	NaN
+## Zeynep	NaN	-0.257199 NaN
+## Atlas	NaN	NaN	-0.089185
+## Mehmet	NaN	-1.382907 NaN
